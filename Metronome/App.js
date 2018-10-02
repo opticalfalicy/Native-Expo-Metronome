f6a780a29cf5dedc724fd609879c12c95a796522
Unknown <opticalfalicy@gmail.com>
2018-10-02 17:02:00 -0500

    Interval working, need to figure out how to return out of it

--- Metronome/App.js
+++ Metronome/App.js
@@ -6,15 +6,58 @@ import Time from './components/Time';
 import Bpm from './components/Bpm';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      countArr: [1, 2, 3, 4],
+      counting: false
+    }
+  }
+  
+  countingOn = () => {
+    console.log('1')
+    // this.setState({ 
+    //     // counting: 1,
+    // });
+    console.log(this.state.counting);
+    // this.testFunc();
+
+    // console.log(this.state.counting)
+    
+    // if(this.state.counting == true){
+    // }
+    // let cntArr = this.state.countArr;
+    // // let i = 0
+    // let repeat = false;
+
+
+
+    // const ubpm = 60000 / 120;
+
+    // // while(this.state.counting == true){
+    //     for (let i = 0; i < cntArr.length; i++) {
+    //         (function (i) {
+    //             setTimeout(function () {
+
+    //                 console.log(cntArr[i]);
+    //         }, ubpm*i);
+    //     })(i);
+    //     console.log('repeat');
+    // }
+    
+
+// }
+}
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.mainView}>
-          <Main style={styles.main}/>
+          <Main counting={this.state.counting} countArr={this.state.countArr} style={styles.main}/>
         </View>
         <View style={styles.bottomView}>
           {/* <Time style={styles.time}/> */}
-          <Bpm />
+          {/* <Bpm /> */}
           {/* <Text style={styles.maincolor}>Open up App.js to start working on your app!</Text> */}
         </View>
       </View>
--- Metronome/components/Main.js
+++ Metronome/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { PlaySound, StopSound, PlaySoundRepeat, PlaySoundMusicVolume } from 'react-native-play-sound';
 
+
 // const SOUNDLIST = [
 //     new SoundlistItem(
 //         'block',
@@ -9,216 +10,111 @@ import { PlaySound, StopSound, PlaySoundRepeat, PlaySoundMusicVolume } from 'rea
 //     )
 // ]
 
+
 export default class Main extends React.Component {
     constructor(props){
         super(props);
-        this.state ={
+        this.state = {
+            counting: true,
             // measure: 4,
             // count: 0,
             countArr: [1, 2, 3, 4],
-            counting: false,
+            // counting: false,
         };
         
-         
-    }
-
-
-    // counter = () => {
-    //     const countArr = this.state.countArr;
-    //     while(this.state.counting != false){
-    //         setTimeout(function(){
-    //                 for(let i = i; i <= countArr.length; i++){
-    //                 console.log([i],
-    //                 this.ubpm
-    //             )                
-    //         }
-    //     })};
         
-
-    // }
-
-    componentDidMount(){
-        this.setState({counting: false})
     }
-
-
-    countingOn = () => {
-        this.setState({ counting: true });
-        let cntArr = this.state.countArr;
-        let i = 0
-        let repeat = false;
-
-        // while(i < countArr.length){
-        //     for(let j = 0; j < countArr.length; j++){
-        //         i++
-        //     }
-        //     console.log(i)
-            
-        // }
-        // console.log(i)
-
-        // for(let i = 0; i < countArr.length + 1; i++){
-        //     if(countArr[i] == countArr.length){
-        //      i = 0;
-        //     }
-        //     console.log(i)
-        // }
-      
     
+    counterHandler = () => {
 
         const ubpm = 60000 / 120;
-        // let i = 0;
-        // this.setState({ counting: !this.state.counting });
+
+        
+        this.setState({ 
+            counting: !this.state.counting
+        });
         // console.log(this.state.counting)
-        // this.counter();
-        // while(this.state.countArr == true) {
-        // arrLoop = () => {
-        while(repeat == false){
-            for (i; i < cntArr.length; i++) {
-                (function (i) {
-                    setTimeout(function () {
-                        if(i == cntArr.length){
-                            repeat = true;
-                        }
-                        console.log(cntArr[i]);
-                }, ubpm*i);
-            })(i);
-            console.log('repeat');
+
+        // var counting = setInterval(function() {
+        //     let countingState
+        // })
+        let i = 0
+
+        if(this.state.counting == true){
+            console.log('t');
+        counting = setInterval(function(){
+                i += 1
+                return i
+                // console.log(i)
+            }, ubpm);
+            console.log(i);
         }
-        
-        //     // if(i == 4){countArr[i] == 0 }
-        // };
-        // }
-    }
-}
+        else if(this.state.counting == false){
+            console.log('f');
+        clearInterval(counting)
+        }
 
-// arrLoop = (repeat) => {
-    //     let cntArr = this.state.countArr;
-        
-    //     let i = 0;
-    //     const ubpm = 60000 / 120;
-    //     let reset = false;
-    //     let count;
-    
-    //     for (i; i < cntArr.length; i++) {
-    //         (function (i) {
-    //             setTimeout(function () {
-    //                 if(i == cntArr.length - 1){
-    //                     reset = true;
-    //                 }
-    //                 return count = cntArr[i]
-    //             }, ubpm*i);
-    //         })(i);
-    //         // return count
-    //     }
-    //     console.log(reset, cntArr[i]);
-    //     console.log(count)
-    // }
+        console.log(i)
 
+        // this.counting = setInterval(function(){
+        //         console.log('x')
+        // }, ubpm);
 
-countingOff = () => {
-    this.setState({counting: false});
-    // console.log(this.state.counting)
-}
-
-
-counter = () => {
-    // for (var i = 0; i < 5; i++) {
-    //     (function (i) {
-    //         setTimeout(function () {
-    //             console.log("hello");
-    //         }, 3000*i);
-    //     })(i);
-    // };  
-    console.log(this.state.counting);
-    if(this.state.counting == true){
-        return console.log('yo')
-    }
-}
-
-        
-        
-        
-        // }
-        
-    // countingOn = () => {
-    //     this.setState({ counting: !this.state.counting });
-        
-    //     const ubpm = 60 / 120;
-    //     let count = 0;
-    //     const countArr = this.state.countArr;
-    //     for(let i = 0; i < countArr.length; i++){
-    //         setTimeout(function(){
-    //             ++count
-    //             console.log('toggle on', count);
-    //         },
-    //             500
-    //         )
-    //     }
-        
-    
-        // while(this.state.counting == true){
-        //         for(let i = i; i <= countArr.length; i++){
-        //             setTimeout(function(){
-        //                     return console.log([i])},
-        //                     ubpm
-        //             )};
-        //         //    return console.log('1');
-        //         }
-                
-                
-    // }
+    }    
 
     // countingOff = () => {
-    //     this.setState({ counting: false });
-    //     console.log('countingOff');
+    //     // clearInterval(counting)
     // }
 
-    // countingOff
+
+
+    componentDidMount(){
+        this.setState({
+            counting: true
+        })
+        
+    }
 
     render(){
         // console.log(this.state.counting);
+        // console.log(this.props.counting);
 
         let textToggle;
         let buttonToggle;
 
-        if (this.state.counting == false){
+        if (this.state.counting == true){
             textToggle = (
                 <Text style={styles.maincolor}>Touch To Start</Text>
             );
             
         } 
-        if (this.state.counting == true) {
+        if (this.state.counting == false) {
             textToggle = (
             <Text style={styles.maincolor}>Touch To Stop</Text>
             );
         }
 
-        if (this.state.counting == false){
-            buttonToggle = (
-        <TouchableOpacity onPress={this.countingOn} style={styles.container}>
-
-                {textToggle}
-                {/* <Text style={styles.maincolor} >Press To Start</Text> */}
-            </TouchableOpacity>
-            )}
-        if (this.state.counting == true){
-            buttonToggle = (
-        <TouchableOpacity onPress={this.countingOff} style={styles.containerOn}>
-
-                {textToggle}
-                {/* <Text style={styles.maincolor} >Press To Start</Text> */}
-            </TouchableOpacity>
-            )}
-
+        // if(this.state.counting == false){
+        //     console.log('button on')
+        //     buttonToggle = this.countingOn;
+        // }
+        // if(this.state.counting == true){
+        //     console.log('button off')
+        //     buttonToggle = this.countingOff;
+        // }
         
-
-
         return(
-            <TouchableOpacity >
 
-               {buttonToggle}
+            <TouchableOpacity onPress={this.counterHandler} style={styles.container}>
+            {textToggle}
+            {/* <Text style={styles.maincolor} >Press To Start</Text> */}
             </TouchableOpacity>
+ 
+        
+            // <TouchableOpacity >
+
+            //    {buttonToggle}
+            // </TouchableOpacity>
         );
     }
 }
@@ -234,12 +130,13 @@ const styles = StyleSheet.create({
         height: 180,
         paddingTop: 70,
     },
-    containerOn:{
+    containerIn:{
         
-        borderColor: 'rgb(250, 255, 255)',
-        borderStyle: 'solid',
-        borderWidth: 5,
-        borderRadius: 100,
+        // borderColor: 'rgb(250, 255, 255)',
+        // borderStyle: 'solid',
+        // borderWidth: 5,
+        // borderRadius: 100,
+        backgroundColor: 'white',
         width: 180,
         height: 180,
         paddingTop: 70,
